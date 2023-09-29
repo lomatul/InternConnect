31508f548bfc63b06067df061caf081dec1e6f81
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-09-30 03:15:02 +0600

    Established Database Connnection
    - Worked on Index.js of the server side

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import studentRoute from "./routes/student.routes.js";
+
+const app = express();
+dotenv.config();
+
+const connection = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'InterConnect', 
+      });
+      console.log("Connected to database successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+app.use(express.json()); 
+app.use(cookieParser());
+app.use(cors({origin:"http://localhost:3000", credentials: true}));
+
+
+app.use("/InterConnect/student", studentRoute);
+
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+  
+    return res.status(errorStatus).send(errorMessage);
+});
+
+
+app.listen(3000, function(){
+    connection();
+    console.log("Server is running on Port 3000");
+});
