59e3aab8a5cc6c0f699c6f12128ed5287bc726fe
GoonerMAK <mahfuzanan20@gmail.com>
2023-09-30 18:06:43 +0600

    Implemented Login

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -1,47 +1,64 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+import bcrypt from 'bcrypt';
 
 const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
 
-    name: { type: String, required: true },
+  student_id: { type: String, unique: true, required: true },
 
-    student_id: { type: String, unique: true, required: true },
+  email: { type: String, required: true },
 
-    email: { type: String, required: true },
+  password: { type: String, required: true },
 
-    password: { type: String, required: true },
+  CV: { type: String },
 
-    CV: { type: String,  },
+  companyPreferences: [{ type: String }],
 
-    companyPreferences: [{ type: String }],
+  domainPreferences: [{ type: String }],
 
-    domainPreferences: [{ type: String }],
+  CGPA: { type: Number },
 
-    CGPA: { type: Number, },
+  currentStatus: { type: String }, //  Selected, In Progress or Rejected
 
-    currentStatus: { type: String },        //  Selected, In Progress or Rejected
+  companyStatus: { type: String }, //   Under which Company
 
-    companyStatus: { type: String },    //   Under which Company
+  OTP: { type: String },
 
-    OTP: { type: String }, 
+  accountActivationStatus: { type: String }, // Account Activated or Deactivated
 
-    accountActivationStatus: { type: String },          // Account Activated or Deactivated
+  submissionTimestampCV: { type: Date },
 
-    submissionTimestampCV: { type: Date }, 
+  weeklyBiWeeklyReport: { type: String },
 
-    weeklyBiWeeklyReport: { type: String }, 
+  internshipReport: { type: String },
 
-    internshipReport: { type: String },
+  presentationMarks: { type: Number },
 
-    presentationMarks: { type: Number }, 
+  finalGrade: { type: String },
 
-    finalGrade: { type: String }, 
+  createdAt: { type: Date, default: Date.now },
 
-    createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
 
-    updatedAt: { type: Date, default: Date.now },
+// Static login method
+studentSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
 
-  });
+  const student = await this.findOne({ email })
+  if (!student) {
+    throw Error('Incorrect email')
+  }
 
-export default mongoose.model('Student', studentSchema);
+  const match = await bcrypt.compare(password, student.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
 
+  return student
+};
 
+export default mongoose.model("Student", studentSchema);
