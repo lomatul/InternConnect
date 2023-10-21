3b333138b7e10a282fbad89f0fdfe30677a1960c
Zayed Hasan <95176649+potato53@users.noreply.github.com>
2023-10-21 23:31:54 +0600

    for merge

---
+++ server/middlewares/multer.Cv.js
@@ -0,0 +1,24 @@
+import multer from 'multer';
+import express from 'express';
+
+const fs = require('fs');
+const path = require('path');
+
+
+const tempDir = path.join(__dirname, 'temp');
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, tempDir);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+
+const upload = multer({ storage: storage });
+
+export default upload;
