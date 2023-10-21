7f24673a321d5dbe64fd0e0f68a9c7a87d47e94f
lomatul <mahzabin@iut-dhaka.edu>
2023-10-21 18:14:07 +0600

    cv upload page done

--- client/src/components/student/cv.jsx
+++ client/src/components/student/cv.jsx
@@ -4,30 +4,49 @@ import "../admin/Add.css";
 const UploadCV = () => {
     return (
       <div className="add">
-           <img
-            src="cv.png"
-            alt="InternConnect Logo"
-            style={{ width: '100px', height: '100px', marginTop:'50px'}}
-        />
         <div className="addcontainer">
-          <h1> Upload your CV here</h1> 
+        <h2 style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="guide.gif" alt="Icon" style={{ width: '60px', height: '60px', marginRight: '10px' }} />
+            CV Guidelines  </h2>
+
+          <div className="cvguideline">           
+                <ul>
+                    <li>Use a clear and professional format for your CV (preferable in Latex).</li>
+                    <li>Include your contact information at the top of the CV.</li>
+                    <li>Highlight your skills, experience, and education.</li>
+                    <li>Tailor your CV for the specific job or internship you're applying for.</li>
+                </ul>
+                <p>For more detailed guidelines, please refer to our <a href="/cv-guidelines">CV Guidelines Page</a>.</p>
+         </div>
+
+         <h2 style={{ display: 'flex', alignItems: 'center'  }}>
+            <img src="cv.gif" alt="Icon" style={{ width: '60px', height: '60px', marginRight: '10px' }} />
+            Upload Your CV  </h2>
        
-          <div className="addsections"  style={{ marginTop:'100px' }}>     
-            <div className="details">           
-            <label htmlFor=""   style={{ color:'red' }} >See Guideline page for details*</label>
-            <div className="xcellupload">
-             
+          <div className="addsections" >   
+            <div className="details">                      
+            <div className="xcellupload">             
               <input type="file" />
               <button>Upload</button>
             </div>       
             </div>           
           </div>
-          <h1>Some Sample CV's</h1> 
-          <div className="sample-cvs">
-                <a href="sample_cv_1.pdf" download>Download Sample CV 1</a>
-                <a href="sample_cv_2.pdf" download>Download Sample CV 2</a>
-                <a href="sample_cv_3.pdf" download>Download Sample CV 3</a>
-          </div>
+
+
+          <h2 style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="samplecv.gif" alt="Icon" style={{ width: '60px', height: '60px', marginRight: '10px' }} />
+            Sample CV's  </h2>
+
+
+                <div className="sample-cvs">
+                        <a href="cvsample1.pdf" download>Download Sample CV 1</a>  
+                    </div>
+                <div className="sample-cvs">
+                        <a href="cvsample2.pdf" download>Download Sample CV 2</a>
+                    </div>
+                <div className="sample-cvs">
+                        <a href="cvsample3.pdf" download>Download Sample CV 3</a>
+                </div>
         </div>
       </div>
     );
