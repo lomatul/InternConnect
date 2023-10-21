ce08cf299921f0d92de77482eb06727381d58dd2
lomatul <mahzabin@iut-dhaka.edu>
2023-10-21 13:22:49 +0600

    sidebar icon added

--- client/src/components/admin/studentsidebar.jsx
+++ client/src/components/admin/studentsidebar.jsx
@@ -6,12 +6,12 @@ function Sidebar({openSidebarToggle, OpenSidebar}) {
     <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
         <div className='sidebar-title'>
             <div className='sidebar-brand'>
-            {/* <img
-            src="logo.png"
+            <img
+            src="Ourlogobk.png"
             alt="InternConnect Logo"
-            style={{ width: '200px', height: '200px' }}
-        /> */}
-                 InternConnect
+            style={{ width: '150px', height: '150px' }}
+        />
+             
             </div>
             <span className='icon close_icon' onClick={OpenSidebar}>X</span>
         </div>
@@ -20,25 +20,32 @@ function Sidebar({openSidebarToggle, OpenSidebar}) {
           
             <Menu >
          
-                <MenuItem icon={<img src="logo.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
-                    Home</MenuItem>
+                <MenuItem icon={<img src="home.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
+                <a href="/">
+                     Home
+                </a></MenuItem>
             
 
-                    <SubMenu label="Comapany">
+                    <SubMenu label="Comapany" icon={<img src="com.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
                         <MenuItem>See Companylist</MenuItem>
                         <MenuItem>Add Company</MenuItem>
                     </SubMenu>
 
-                    <SubMenu label="Student">
+                    <SubMenu label="Student" icon={<img src="stu.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
                         <MenuItem>See Studentlist</MenuItem>
                         <MenuItem>Add Student</MenuItem>
                     </SubMenu>
 
-                <MenuItem>Add Guideline</MenuItem>
-
-                <MenuItem>CV Sending</MenuItem>
-                <MenuItem>Notification</MenuItem>
-                <MenuItem>LogOut</MenuItem>
+                    <MenuItem icon={<img src="guide.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
+                    <a href="/AddguideLine">
+                     Add Guideline
+                </a></MenuItem>
+                    <MenuItem icon={<img src="cvcv.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
+                        CV Sending</MenuItem>
+                    <MenuItem icon={<img src="notifi.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
+                        Notification</MenuItem>
+                    <MenuItem icon={<img src="logout.png"alt="InternConnect Logo"   style={{ width: '20px', height: '20px' }}   />}>
+                        LogOut</MenuItem>
 
             </Menu>
       
