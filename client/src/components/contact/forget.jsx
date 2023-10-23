9212570dbbe1e0f072a735a6b8b12ad968d2efe6
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-10-23 23:42:04 +0600

    Couple Exceptions Handled - Login, Forget Password
    - Added a function that handles input change and allows only digits in case of Student IDs
    
    - Logic: (student_id.length === 9 && student_id.startsWith('2'))
    
    - Known Issue: Couldn't check whether the student ID exists in the database or not while updating password (Forget Password page)

--- client/src/components/contact/forget.jsx
+++ client/src/components/contact/forget.jsx
@@ -8,30 +8,51 @@ import { useNavigate } from 'react-router-dom';
 function Forgets() {
   const navigate = useNavigate();
   const [student_id, setStudent_id] = useState('')
+  const [idError, setIdError] = useState('');
 
+// Function to handle input change and allow only digits
+const handleStudentIdChange = (e) => {
+  const inputValue = e.target.value;
+  const regex = /^\d*$/;            // Regular expression to match digits only
+  if (regex.test(inputValue)) {
+    setStudent_id(inputValue);
+  }
+}
 
   const handleSubmit = async(event) => {
     event.preventDefault()
-    try {
-        await axios.post('http://localhost:4000/InterConnect/student/forgetPassword', {student_id}
-        ).then((response)=>{
-            console.log(response)
-            
-            navigate("/Updatepassword",{state: {Id:student_id, from:1}})
-        }).catch((error)=>{
-            if (error.response) {
-                console.log(error.response);
-                console.log("server responded");
-              } else if (error.request) {
-                console.log("network error");
-              } else {
-                console.log(error);
-              }
-        });
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
+
+    if(student_id.length === 9 && student_id.startsWith('2')) {
+      setIdError('');
+
+      try {
+          await axios.post('http://localhost:4000/InterConnect/student/forgetPassword', {student_id}
+          ).then((response)=>{
+              console.log(response)
+
+              navigate("/Updatepassword",{state: {Id:student_id, from:1}})
+          }).catch((error)=>{
+              if (error.response) {
+                  console.log(error.response);
+                  console.log("server responded");
+                } else if (error.request) {
+                  console.log("network error");
+                } else {
+                  console.log(error);
+                }
+          });
+        } catch (error) {
+          console.error('An error occurred:', error);
+        }
+    }
+
+    else {
+      setIdError("Invalid Student ID. It should be 9 digits long and start with '2'");
+    }
+
   }
+
+
   return (
     <main>
       <article>
@@ -47,16 +68,16 @@ function Forgets() {
             <form action="" className="contact-form">
            
 
-              <label htmlFor="">Student ID <span>*</span></label>
-              <input type="Text" min={0} value={student_id} onChange={(e) => setStudent_id(e.target.value)}/>
-  
+            <label htmlFor="">Student ID <span>*</span></label>
+            <input type="Text" min={0} value={student_id} onChange={handleStudentIdChange}/>
+            <span style={{ color: 'red' }}>{idError}</span> 
               
 
-          <button onClick={handleSubmit}>
+          <button onClick={handleSubmit} style={{ cursor: 'pointer' }}>
             Send OTP
           </button>
-          <button >
-            <a href="/">Go Back</a>
+          <button style={{ cursor: 'pointer' }}>
+            <a href="/" >Go Back</a>
           </button>
 
         </form>
