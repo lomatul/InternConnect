3cc235e48c3fd9cd8e2e91ba87ba39d350a2ff89
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-10-24 21:11:11 +0600

    New mail added (interconnect.swe@gmail.com)
    - The project now has an official mail

--- server/controllers/mailsender.js
+++ server/controllers/mailsender.js
@@ -10,12 +10,12 @@ const Mailfunction = async (Name, email, otp) =>{
             service:'gmail',
             host: "smtp.gmail.com",
             auth: {
-                user: "zayed.hasan.14@gmail.com",
+                user: "internconnect.swe@gmail.com",
                 pass:process.env.mail_pass
             }
         })
         await transporter.sendMail({
-            from: "zayed.hasan.14@gmail.com",
+            from: "internconnect.swe@gmail.com",
             to: email,
             subject:  'Email Verification',
             text:`Hi ${Name}, Please, Click the following link to Login using your OTP. And update the password to activate your account. And your OTP is '${otp}'`,
