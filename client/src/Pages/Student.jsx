dfab2978e712e0916bfd2d257a609bed9f5c677c
lomatul <mahzabin@iut-dhaka.edu>
2023-10-31 00:01:04 +0600

    student Dashboard frontend fixed

--- client/src/Pages/Student.jsx
+++ client/src/Pages/Student.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import '../components/admin/Dashboard.css'
-import Sidebar from '../components/admin/studentsidebar'
-import StudentProfile from '../components/student/studentprofile'
-
+import '../components/test.css'
+import Navbar from "../components/Navbar/navbar"
+import '../components/Navbar/navbar.css'
+import StudentDashboard from '../components/student/studentDashboard'
 
 function Students() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
-
-  const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
-  }
-
+ 
   return (
-    <div className='AdminDashboard'>   
-          {/* <Header OpenSidebar={OpenSidebar}/> */}
-      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-      <StudentProfile />
+    <div className='newAdmin'>   
+   <Navbar/>
+   <StudentDashboard/>
     </div>
   )
 }
--- client/src/components/student/studentDashboard.jsx
+++ client/src/components/student/studentDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Dashboard() {
+function StudentDashboard() {
 
   return (
    
@@ -8,11 +8,9 @@ function Dashboard() {
     <div className='admincontainer'>
         <div className='text'>
             <h3>Welcome to  </h3>
-            <h1>Admin Dashboard</h1>
+            <h1>Student Dashboard</h1>
         </div>
-        {/* <div className='image'>
-            <img src="adminadmin.gif" alt="" />
-        </div> */}
+
     </div>
 
     <div className='admincard'>
@@ -20,27 +18,27 @@ function Dashboard() {
             <div className='cards'>
                 
                 <div className='card'>
-                    <img src="ad-stu.png" alt=""  />
-                    <a href="/AddStudent">Add Students &rarr;</a>
+                    <img src="stu-profile.png" alt=""  />
+                    <a href="/AddStudent">Profile &rarr;</a>
                 </div>
 
                 <div className='card'>
                     <img src="ad-com.png" alt=""  />
-                    <a href="/AddCompany">Add Companies &rarr;</a>
+                    <a href="/AddCompany">Company list  &rarr;</a>
                 </div>
 
 
                 <div className='card'>
-                    <img src="ad-guide.png" alt=""  />
-                    <a href="/Addguideline">Add Guideline &rarr;</a>
+                    <img src="stu-prefer.png" alt=""  />
+                    <a href="/Addguideline">Add Preferences &rarr;</a>
                 </div> 
             </div>
 
             <div className='cards'>
                 
                 <div className='card'>
-                    <img src="ad-cv.png" alt=""  />
-                    <a href="/">Send CV &rarr;</a>
+                    <img src="cv-up.png" alt=""  />
+                    <a href="/">CV Upload &rarr;</a>
                 </div>
 
                 <div className='card'>
@@ -62,4 +60,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard
+export default StudentDashboard
