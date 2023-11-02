82777af71fa8c2fe10aaa00187bc07aac032349d
lomatul <mahzabin@iut-dhaka.edu>
2023-11-02 19:03:38 +0600

    new frontend added

--- client/src/components/student/preference.jsx
+++ client/src/components/student/preference.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState }  from 'react'
 import {useAuthContext} from "../../context/useAuthcontext";
 import axios from "axios";
 import Select from 'react-select';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Prefernces = () => {
