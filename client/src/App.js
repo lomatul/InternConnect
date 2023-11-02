5193441347796cfb16407c9912744665764f7937
lomatul <mahzabin@iut-dhaka.edu>
2023-11-02 23:47:37 +0600

    new login frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import Contacts from "./components/contact/contact";
 import Forgets from "./components/contact/forget";
 import Students from "./Pages/Student";
 import Companies from "./Pages/CompanyList";
-import AdminLogin from "./components/login/Adminlogin";
 import AddStudent from "./Pages/AddStudent";
 import Addcv from "./Pages/AddCV";
 import Addprefer from "./Pages/Addprefenrece";
@@ -50,7 +49,6 @@ function App() {
         <Route path="/Forget" element = {<Forgets/>} />
         <Route path="/Select" element = {<Dropdown/>} />
         <Route path="/Student" element = {userstudent?<Students/>:<Page404/>} />
-        <Route path="/Adminlogin" element = {<AdminLogin/>} />
         <Route path="/AddStudent" element = {useradmin?<AddStudent/>:<Page404/>} />
         <Route path="/NotFound" element = {<Page404/>} />
         <Route path="/AddCV" element = {userstudent?<Addcv/>:<Page404/>} />
--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,17 +1,85 @@
 import React from 'react';
 
-  import {  toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  
+import { useState } from "react";
   function Test(){
-    const notify = () => toast.success("Wow so easy!");
 
-    return (
-      <div>
-        <button onClick={notify}>Notify!</button>
-       
-      </div>
-    );
-  }
+    const [isAdminMode, setIsAdminMode] = useState(false);
+
+    const handleSignUpClick = () => {
+      setIsAdminMode(true);
+    };
+  
+    const handleSignInClick = () => {
+      setIsAdminMode(false);
+    };
+
+
+
+return (
+<div className={`logincontainer ${isAdminMode ? 'admin-mode' : ''}`}>
+      <div className="forms-logincontainer">
+            <div className="signin-signup">
+              <form action="#" className="admin-form">
+                <h2 className="title">Sign in</h2>
+                <div className="input-field">
+                  <i className="fas fa-user"></i>
+                  <input type="text" placeholder="Username" />
+                </div>
+                <div className="input-field">
+                  <i className="fas fa-lock"></i>
+                  <input type="password" placeholder="Password" />
+                </div>
+
+              </form>
+              <form action="#" className="student-form">
+                <h2 className="title">Sign up</h2>
+                <div className="input-field">
+                  <i className="fas fa-user"></i>
+                  <input type="text" placeholder="Username" />
+                </div>
+                <div className="input-field">
+                  <i className="fas fa-envelope"></i>
+                  <input type="email" placeholder="Email" />
+                </div>
+                <div className="input-field">
+                  <i className="fas fa-lock"></i>
+                  <input type="password" placeholder="Password" />
+                </div>
+
+              </form>
+            </div>
+          </div>
+    
+          <div className="panels-logincontainer">
+            <div className="panel left-panel">
+              <div className="logincontent">
+                <h3>New here ?</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                  ex ratione. Aliquid!
+                </p>
+                <button className="btn transparent" id="sign-up-btn" onClick={handleSignUpClick}>
+                  Sign up
+                </button>
+              </div>
+              <img src="img/log.svg" className="image" alt="" />
+            </div>
+            <div className="panel right-panel">
+              <div className="logincontent">
+                <h3>One of us ?</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                  laboriosam ad deleniti.
+                </p>
+                <button className="btn transparent" id="sign-in-btn" onClick={handleSignInClick}>
+                  Sign in
+                </button>
+              </div>
+              <img src="img/register.svg" className="image" alt="" />
+            </div>
+          </div>
+        </div>
+      );
+    };
 
   export default Test
