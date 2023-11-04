d4a088f51753e7bba8789af111bb16da2b0fe82d
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-11-05 04:24:16 +0600

    Added Toast Operations
    - Confirms and Shows error while creating students, sending notifications, editing student profile (Doesn't work when the save button is pressed in the student profile page - known issue)
    
    - The Known Issue: Code doesn't reach the code that handle toast operations in Student Profile page.

--- client/src/components/student/studentprofile.jsx
+++ client/src/components/student/studentprofile.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 // import "./Add.css";
 import {useAuthContext} from "../../context/useAuthcontext"
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // const user = JSON.parse(localStorage.getItem('user')) || {"student_id" : "100"};
 // const student_id = user.student_id
@@ -91,8 +93,8 @@ const StudentProfile = () => {
 
 
   const handleEditClick = () => {
-    // Enable edit mode
-    setEditMode(true);
+    setEditMode(true);              // Enable edit mode
+    toast.info('Editing profile', { position: "top-right" });
   };
 
   const handleimagesave = () => {
@@ -143,10 +145,13 @@ const StudentProfile = () => {
         });
         // Handle the response if needed
         console.log(response.data); // This is just for demonstration
-        window.location.reload();
-      } catch (error) {
+        toast.success('Profile saved successfully', { position: "top-right" });
+        // window.location.reload();
+      }
+       catch (error) {
         // Handle errors
         console.error(error);
+        toast.error('Error while saving profile', { position: "top-right" });
       }
   
     }catch(error){
