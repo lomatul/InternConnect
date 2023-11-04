fee49b9c38d8e1a034115dbbbb228352bd33c019
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-11-05 03:20:09 +0600

    Modified fronted integration code for AddCompany
    - Added a default value of year in the Company Model
    - Properly Handling Contract Number (Was flawed Before)
    - Changed the form data because of the new change in the Company Model (Historical data)

--- client/src/components/admin/AddCompany.jsx
+++ client/src/components/admin/AddCompany.jsx
@@ -1,121 +1,255 @@
 import React, { useState } from 'react';
 import "./Add.css";
 import axios from "axios";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
 
 
 const Add = () => {
+  const [selectedFile, setSelectedFile] = useState([]);
+  const [emailError, setEmailError] = useState('');
+  const [contactNumberError, setContactNumberError] = useState('');
+
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0])
+    console.log("file", selectedFile)
+  }
+
+
+  const isEmailValid = (email) => {
+    const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+(?:\.[a-z]{2,4}){1,}$/i;
+    return emailPattern.test(email);
+  };
+
 
   const [formData, setFormData] = useState({
-    companyID: "",
     name: "",
     address: "",
-    email: "",
-    requiredDomain: "",
+    historicalData: [
+      {
+        year: new Date().getFullYear(),
+        address: "",
+        requiredDomain: [],
+        internsHired: 0,
+        contactNumber: "",
+        selectedInterns: [],
+      },
+    ],
     minInterns: "",
     maxInterns: "",
-    internsHired: "",
-    contactNumber: "",
     status: "",
+    description: ""
   });
+
+
+  const handlefileSubmit = async(event) => {
+    event.preventDefault()
+  
+    const formDatafile = new FormData();
+    formDatafile.append("file", selectedFile);
+    console.log(formDatafile)
+    try {
+        await axios.post('http://localhost:4000/InterConnect/admin/uploadcompanyfile', formDatafile, {
+          headers: {
+            'Content-Type': 'multipart/form-data', // Set the content type for file upload
+          },
+        }).then((response)=>{
+            console.log(response)
+         
+        }).catch((error)=>{
+            if (error.response) {
+                console.log(error.response);
+                console.log("server responded");
+                toast.error('File upload failed', { position: "top-right" });
+              } else if (error.request) {
+                console.log("network error");
+              } else {
+                console.log(error);
+              }
+        });
+  
+        // if (response.status === 200) {
+        //   console.log('File uploaded successfully');
+        // } else {
+        //   console.error('File upload failed');
+        // }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+      setSelectedFile(null)
+  }
+
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+    
   };
+
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    // Check if the email is valid
+    if (!isEmailValid(formData.email)) {
+      setEmailError('Please enter a valid email address');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+    // Check if the contact number is valid
+    const contactNumberPattern = /^01\d{9}$/; // Pattern for contact number starting with "01" and having 11 digits
+    if (!contactNumberPattern.test(formData.contactNumber)) {
+      setContactNumberError('Contact number should start with "01" and have 11 digits in total');
+      return;
+    } else {
+      setContactNumberError('');
+    }
+
+
     try {
       const response = await axios.post('http://localhost:4000/InterConnect/company/createCompany', formData);
       console.log('Response:', response.data);
+      toast.success('Company created successfully', { position: "top-right" });
       // Handle success and error cases
     } catch (error) {
       console.error('An error occurred:', error);
+      toast.error('Failed to create the company', { position: "top-right" });
       // Handle the error cases
     }
+
+    setFormData({
+      companyID: "",
+      name: "",
+      historicalData: [
+        {
+          year: new Date().getFullYear(),
+          address: "",
+          requiredDomain: [],
+          internsHired: 0,
+          contactNumber: "",
+          selectedInterns: [],
+        },
+      ],
+      minInterns: "",
+      maxInterns: "",
+      status: "",
+      description: ""
+    })
   };
 
 
     return (
-      <div className="add">
-        <div className="addcontainer">
-          <h1>Add New Company</h1>
+      <div >
+          <div className='admincontainer'>
+          <div className='studenttext'>
+              <h3>Streamline Your Internship Program</h3>
+              <h1>Effortlessly Add Companies with Excel Upload!</h1>
+            
+          </div>
+          <div className='adminimage'>
+              <img src="adcompany.gif" alt="" />
+          </div>
+      </div>
+      
+      <div className="studentguideline">           
+                  <ul>
+                     
+                      <p> Administrators play a crucial role in creating, updating, and managing the company pool to facilitate student access to internship opportunities and align the system with the changing needs of both students and companies.For more detailed guidelines, please refer to our <a href="/Guildeline">Guidelines Page</a>.</p>
+                  </ul>
+                 
+             <div className='xcellupload'>         
+                   <input type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" onChange={handleFileSelect}/>
+                    <button onClick={handlefileSubmit}>Create</button>          
+             </div>
+       </div>
+
+       <div className='admincontainer'>
+          <div className='studenttext'>
+              <h1>Seamless Company Addition: Step by Step </h1>
+             
+          </div>
+          <div className='adminimage'>
+              <img src="comform.gif" alt="" />
+          </div>
+      </div>
+
+       <div className="studentpreference">
           <form onSubmit={handleSubmit}>
-          <div className="addsections">
-          {/* <div className="info">
-          </div>  */}
-  
-            <div className="details">
-            <label htmlFor="">Add Companies by Xcell Upload</label>
-            <div className="xcellupload">
-              <input type="file" />
-              <button>Create</button>
-            </div>
-          
+
+          <div className="form-columns">
+          <div className="form-group">
               <label htmlFor="name">Company Title<span>*</span></label> 
               <input type="text" name="name" placeholder="Give Company name" value={formData.name} onChange={handleChange}/>
-  
-              <label htmlFor="">Short Description<span>*</span></label>
-              <textarea name="" id="" placeholder="Short description of the company" cols="30" rows="10"></textarea>
-  
+          </div>
+
+      
+          <div className="form-group">
               <label htmlFor="address">Address<span>*</span></label>
               <input type="text" name="address" value={formData.address} onChange={handleChange} />
+          </div>
 
+
+          <div className="form-group">
               <label htmlFor="email">Email <span>*</span></label>
-              <input type="text" name="email" value={formData.email} onChange={handleChange} />
-
-              <label htmlFor="contactNumber">Contact Number<span>*</span></label>
-              <input type="number" name="contactNumber" min="0" value={formData.contactNumber} onChange={handleChange} />
-
-              <label htmlFor="maxInterns">Max Interns</label>
-              <input type="number" name="maxInterns" min="0" value={formData.maxInterns} onChange={handleChange} />
-
+              <input type="email" name="email" value={formData.email} onChange={handleChange} />
+              {emailError && <div class="error-message">{emailError}</div>} 
+              </div>
+              <div className="form-group">
               <label htmlFor="minInterns">Min Interns</label>
               <input type="number" name="minInterns" min="0" value={formData.minInterns} onChange={handleChange} />
+          </div>
 
+
+          <div className="form-group">
+              <label htmlFor="contactNumber">Contact Number<span>*</span></label>
+              <input type="text" name="contactNumber" value={formData.contactNumber} onChange={handleChange} />
+              {contactNumberError && <div class="error-message">{contactNumberError}</div>} 
+          </div>
+
+
+          <div className="form-group">
+              <label htmlFor="maxInterns">Max Interns</label>
+              <input type="number" name="maxInterns" min="0" value={formData.maxInterns} onChange={handleChange} />
+          </div>
+
+
+          <div className="form-group">
               <label htmlFor="internsHired">Interns Hired</label>
               <input type="number" name="internsHired" min="0" value={formData.internsHired} onChange={handleChange} />
+          </div>
+          
 
-              {/* <label htmlFor="selectedInterns">Selected Interns</label>
-              <input type="number" name="selectedInterns" min="0" value={formData.selectedInterns} onChange={handleChange} /> */}
-             
+          <div className="form-group">
               <label htmlFor="requiredDomain">Domain</label>
               <div className="multiselect">
                 <div className="select-box">
                   <div className="options-container">
-                    <div className="option">
-                      <input type="checkbox" id="UI/UX Designer" name="requiredDomain" value="UI/UX Designer" onChange={handleChange} />
-                      <label htmlFor="UI/UX Designer">UI/UX Designer</label>
-                    </div>
-                    <div className="option">
-                      <input type="checkbox" id="Software Development" name="requiredDomain" value="Software Development" onChange={handleChange} />
-                      <label htmlFor="Software Development">Software Development</label>
-                    </div>
-                    <div className="option">
-                      <input type="checkbox" id="Documentation" name="requiredDomain" value="Documentation" onChange={handleChange} />
-                      <label htmlFor="Documentation">Documentation</label>
-                    </div>
-                    <div className="option">
-                      <input type="checkbox" id="DevOps" name="requiredDomain" value="DevOps" onChange={handleChange} />
-                      <label htmlFor="DevOps">DevOps</label>
-                        </div>
+
+                  <label> <Checkbox id="UI/UX Designer" name="requiredDomain" value="UI/UX Designer" onChange={handleChange} />  &nbsp; UI/UX Designer </label> 
+                  <label> <Checkbox id="Software Development" name="requiredDomain" value="Software Development" onChange={handleChange} />  &nbsp; Software Development </label> 
+                  <label> <Checkbox id="Documentation" name="requiredDomain" value="Documentation" onChange={handleChange} />  &nbsp; Documentation </label> 
+                  <label> <Checkbox id="DevOps" name="requiredDomain" value="DevOps" onChange={handleChange} />  &nbsp; DevOps </label> 
+
                       </div>
                     </div>
                   </div>
-                  {/* <label htmlFor="">Status </label>
-                      <select >
-                        <option value="running ">Running</option>
-                        <option value="Closed">Closed </option>
-                      </select> */}
-
-              <button type="submit">Create</button>
+              </div>
             </div>
+        
+          <button type="submit">Create</button>
             
-          </div>
+            
           </form>
-        </div>
-      </div>
+          </div>
+            </div>
+
+      
     );
   };
 export default Add
--- server/models/company.model.js
+++ server/models/company.model.js
@@ -2,44 +2,35 @@ import mongoose from 'mongoose';
 
 const companySchema = new mongoose.Schema({
 
-  companyID: { type: String, unique: true },
-
   name: { type: String, required: true },
   
-  address: { type: String, },
-  
-  email: { type: String, required: true, unique: true },
-  
-  requiredDomain: [{ type: String }],       // Array of required domains
+  email: { type: String, required: true, unique: true  },
+
+  historicalData: 
+  [
+    {
+      year: { type: Number, required: true, unique: true, default: new Date().getFullYear() },
+
+      address: { type: String },
+
+      requiredDomain: [  {  domain: { type: String },   internsNeeded: { type: Number, default: 0 }  } ],     // Array of required domains
+
+      internsHired: { type: Number, default: 0 },     // Default to 0
+
+      contactNumber: { type: String },
+
+      selectedInterns: [{ type: mongoose.Schema.Types.ObjectId, ref: 'students' }],   // Array of selected interns (references to Student model)  The collection that's being referenced: students
+
+    }
+  ],
   
   minInterns: { type: Number, },
   
   maxInterns: { type: Number, },
-  
-  internsHired: { type: Number, default: 0 },       // Default to 0
-  
-  contactNumber: { type: String },             
-  
-  selectedInterns: [{ type: mongoose.Schema.Types.ObjectId, ref: 'students' }],   // Array of selected interns (references to Student model)  The collection that's being referenced: students
-  
+      
   status: { type: String },       // Status (Hiring or Closed etc)
 
 });
 
-
-// Pre-saved middleware to generate companyID (Automatically Generated)
-companySchema.pre('save', async function (next) {
-  if (!this.isNew) {
-    return next(); 
-  }
-
-  // Finding the highest existing companyID
-  const highestCompany = await this.constructor.findOne({}, 'companyID').sort({ companyID: -1 }).exec();
-  const highestID = highestCompany ? parseInt(highestCompany.companyID) : 0;
-
-  // Setting the new companyID by incrementing the highest ID
-  this.companyID = (highestID + 1).toString();
-  next();
-});
 
 export default mongoose.model('Company', companySchema);
