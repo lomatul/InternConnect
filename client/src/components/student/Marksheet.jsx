f4d029877b7afe559afbcdd7f0241370c8f6946d
Zayed Hasan <95176649+potato53@users.noreply.github.com>
2024-01-15 13:12:10 +0600

    View Report Card

--- client/src/components/student/Marksheet.jsx
+++ client/src/components/student/Marksheet.jsx
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReportPdf from './ReportViewer'
 
 function Studentmark() {
+  const [response, setResponse]=useState(null)
 
+  
+  useEffect(()=>{
+    try {
+        console.log("came here at deadline")
+        axios.get('http://localhost:4000/InterConnect/Student/ViewGrade', {
+          withCredentials: true
+        }).then((response)=>{
+          console.log(response.data.returnStudentGrade.presentationMarks)
+          setResponse(response.data.returnStudentGrade)
+      }).catch((error)=>{
+          if (error.response) {
+              console.log(error.response);
+              console.log("server responded");
+            } else if (error.request) {
+              console.log("network error");
+            } else {
+              console.log(error);
+            }
+      });
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    }, []
+  )
   return (
        <div >
     <div className='admincontainer'>
@@ -14,7 +40,8 @@ function Studentmark() {
     </div>
 
     <div className='admincard'>
-       
+      {response&&<ReportPdf Marks={response}/>}
+
     </div>
 
 </div>
