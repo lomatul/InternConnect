a45c50bf1a925e0c2a92cd37e1cf32b2c2dbc527
Zayed Hasan <95176649+potato53@users.noreply.github.com>
2024-01-17 01:12:09 +0600

    Testing try

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,5 +1,26 @@
+import {afterAll, describe, expect, it} from "@jest/globals";
+import * as app from ".././index.js"
+import  request  from "supertest";
+
 describe('Sanity test', () => {
     test('1 should equal 1', () => {
       expect(1).toBe(1)
     })
   })
+
+  describe('Login', () => {
+    test('should return hello world object', async () => {
+      const requestBody = {
+        student_id: '200042173',
+        password: 'An@n1234'
+      };
+  
+      const res = await request(app)
+        .post('/InterConnect/student/postlogin')
+        .send(requestBody);
+  
+      expect(res.statusCode).toEqual(200);
+      // Add more assertions based on your response
+      
+    });
+  });
